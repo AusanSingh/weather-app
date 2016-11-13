2a6188e374ac6b0dde1fa0be08f0b2ffa49c3a0c
AusanSingh <123ausan@gmail.com>
2016-11-13 16:04:54 +0530

    weather app setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1 class="text-center">My First Angular App thanks udemy</h1>',
+})  
+export class AppComponent { }
